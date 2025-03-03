50d83052e4b5c019368e37013cf05640bb4b3453
Nikonova Larisa <97353756+Larrrisa@users.noreply.github.com>
2025-03-03 22:18:42 +0400

    feat: add header

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -9,7 +9,7 @@ export const theme = createTheme({
       default: "#0F0F0F",
     },
     text: {
-      primary: "#FFFFFF",
+      primary: "#C3C3C3",
     },
   },
   typography: {
@@ -27,6 +27,32 @@ export const theme = createTheme({
     },
     button: {
       fontSize: 16,
+    },
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        "*": {
+          padding: 0,
+          margin: 0,
+          boxSizing: "border-box",
+        },
+        html: {
+          fontSize: "62.5%",
+        },
+        body: {
+          backgroundColor: "#0F0F0F",
+          padding: "2rem 5rem",
+        },
+        "a, a:visited": {
+          textDecoration: "none",
+          color: "#C3C3C3",
+          cursor: "pointer",
+          "&:hover": {
+            color: "#FFFFFF",
+          },
+        },
+      },
     },
   },
 });
